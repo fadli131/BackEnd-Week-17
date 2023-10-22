675761ea5582e5c55dd8a8f2a35bed4f246398fc
fadli131 <fadliaryadinata011@gmail.com>
2023-10-22 19:06:43 +0700

    first

---
+++ index.js
@@ -0,0 +1,21 @@
+const functions = require('firebase-functions')
+const express = require('express')
+const dotenv = require ('dotenv')
+const useMiddleware = require('./middleware');
+const router = require('./routes');
+
+
+const app = express()
+dotenv.config()
+
+useMiddleware(app)
+
+app.use(router)
+
+const server_port = (process.env.SERVER_PORT) 
+
+app.listen(server_port, () => {
+ console.log(`Running on port http://localhost:${server_port}`);
+})
+
+exports.fadliweek_17 = functions.https.onRequest(app);
