b7ef4f3120d4fb2d4099e313b5f7c6af1b1813c0
fadli131 <fadliaryadinata011@gmail.com>
2023-10-22 20:23:16 +0700

    first

--- db/index.js
+++ db/index.js
@@ -1,19 +1,14 @@
 const { MongoClient } = require('mongodb');
-const dotenv = require("dotenv");
 
 const dbConnection = async () => {
     try {
-        const client = await new MongoClient(process.env.DB_PROD).connect();
+        const client = await MongoClient.connect(process.env.DB_PROD, { useUnifiedTopology: true });
         const db = client.db('week-17');
-        
         const booksCollection = db.collection('Books');
-        const usersCollection = db.collection('Users'); 
-        
+        const usersCollection = db.collection('Users');
         return { db, booksCollection, usersCollection };
     } catch (error) {
         console.log(error, "<=================== error ==================");
         throw new Error("Database connection error");
     }
 }
-
-module.exports = dbConnection;
